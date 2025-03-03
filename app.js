68772a7fd66ed37b51bef782ea7a3979eb32d1ed
LeeMyoungHan <mhyhl654@gmail.com>
2025-03-03 15:24:43 +0900

    mysql 커넥션풀 적용

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const coolsms = require('coolsms-node-sdk').default;
 const randomstring = require('randomstring');
 require('dotenv').config()
 const messageService = new coolsms(process.env.COOLSMSAPI, process.env.COOLSMSAPISECRET)
-const mysql=require('mysql2')
+const mysql=require('mysql2/promise')
 const bcrypt = require('bcrypt');
 var bodyParser = require('body-parser');
 const { errorMonitor } = require('events');
@@ -97,17 +97,97 @@ const fileUpload = multer({
     fileSize: 10 * 1024 * 1024,  // 5MB 로 크기 제한
   },
 })
-
-var db_config ={
-    host: 'localhost',
-    user: "root",
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: "3306",
-    charset:"utf8mb4",
-    dateStrings:'date'
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: "root",
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  connectionLimit: 5,
+  port: "3306",
+  charset:"utf8mb4",
+  dateStrings:'date'
+})
+const getConnection = async () => {
+  try {
+      const conn = await pool.getConnection();
+      return conn;
+  } catch (error) {
+      console.error(`connection error : ${error.message}`);
+      return null;
+  }
 }
-var connection; 
+const releaseConnection = async (conn) => {
+  try {
+      await conn.release();
+  } catch (error) {
+      console.error(`release error : ${error.message}`);
+  }
+};
+const transaction = async (logic) => {
+  let conn = null;
+  try {
+      conn = await getConnection();
+      await conn.beginTransaction();
+
+    //connection만 넣어준다.
+      const result = await logic(conn);
+
+      await conn.commit();
+      return result;
+  } catch (err) {
+      if (conn) {
+          conn.rollback();
+      }
+
+      console.error(err);
+      return null;
+  } finally {
+      if (conn) {
+          releaseConnection(conn);
+      }
+  }
+}
+const FIND_USER_BY_NICKNAME = 'SELECT platform FROM user WHERE nickname = ?'
+
+const find_user_by_nickname = async (conn, nickname) => {
+    try {
+          const [rows] = await conn.execute(FIND_USER_BY_NICKNAME, [nickname]);
+          return rows;
+        } catch (err) {
+          throw err;
+        }
+}
+
+const getplatform = (nickname) => {
+  return async (conn) => {
+    try {
+      const find_platform = await find_user_by_nickname(conn,nickname)
+
+      return find_platform[0].platform
+    } catch(err) {
+      throw err
+    }
+  }
+}
+async function run() {
+  const res1 = await transaction(getplatform(""))  
+  console.log(res1)
+}
+async function sqlquery(query,param,success,error) {
+  const conn = await getConnection()
+  try {
+    let [rows,fields] = await conn.query(query,param)
+    success(rows)
+  
+  
+  } catch(err){
+    console.log(err)
+    error()
+  }
+  
+  conn.release()
+}
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 
@@ -140,29 +220,9 @@ function verifyToken(token,error,success) {
     }
   })
 }
-function handleDisconnect() {
-    connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                    // the old one cannot be reused.
-  
-    connection.connect(function(err) {              // The server is either down
-      if(err) {                                     // or restarting (takes a while sometimes).
-        console.log('error when connecting to db:', err);
-        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-      }                                     // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meantime.
-                                            // If you're also serving http, display a 503 error.
-    connection.on('error', function(err) {
-      console.log('db error', err);
-      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-        handleDisconnect();                         // lost due to either server restart, or a
-      } else {                                      // connnection idle timeout (the wait_timeout
-        throw err;                                  // server variable configures this)
-      }
-    });
-  }
-  handleDisconnect()
+
  
-var port = process.env.PORT || 3000
+
 
 function getCode() {
     let number = Math.floor(Math.random() * 1000000)+100000; // ★★난수 발생 ★★★★★
@@ -199,20 +259,20 @@ app.use(express.json())
       updateProfile = 'update user set nickname=?,gender=?,birthyear=?,profileimage=? where platform=? and account=?'
     }
 
+  
     console.log(param)
-    connection.query(updateProfile,param,function(err,result){
-      if(err) {
-        res.json({
-          isTokenValid:true,
-          resultCode:400
-        })
-      } else {
-        res.json({
-          isTokenValid:true,
-          resultCode:200
-        })
-      }
-    }) 
+    sqlquery(updateProfile,param,function(rows){
+      res.json({
+        isTokenValid:true,
+        resultCode:200
+      })
+    },
+    function() {
+      res.json({
+        isTokenValid:true,
+        resultCode:400
+      })
+    })
   }
  )
 })
@@ -229,73 +289,74 @@ app.post('/requestPhoneAuthCode',(req,res)=>{
     console.log(req.body)
     var phonenumber = req.body.phoneNumber
     var param = [phonenumber]
-    connection.query(checkaccount,param,function(err,result) {
-      if(err) {
+    sqlquery(checkaccount,param,function(result) {
+      
+      if(result&&result.length) {
+      
         res.json({
-          isValid: false,
-          resultCode:500
+          isValid: true,
+          resultCode:200
         })
       } else {
-        if(result&&result.length) {
-      
-          res.json({
-            isValid: true,
-            resultCode:200
-          })
-        } else {
-          var num = getCode()
+        var num = getCode()
     
-          console.log(num)
-          cache.del(phonenumber)
-          cache.put(phonenumber,num)
-          //문자 전송
-          messageService.sendOne(
-            {
-              to: '01057135288', 
-              from: '01057135288', 
-              text: "[고민앱] 인증번호 ["+num+"]를 입력해주세요."
-            }
-          ).then(res => console.log(res))
-          .catch(err => console.error(err))
-          res.json({
-            isValid: false,
-            resultCode:200
-          })
-        }
+        console.log(num)
+        cache.del(phonenumber)
+        cache.put(phonenumber,num)
+        //문자 전송
+        messageService.sendOne(
+          {
+            to: '01057135288', 
+            from: '01057135288', 
+            text: "[고민앱] 인증번호 ["+num+"]를 입력해주세요."
+          }
+        ).then(res => console.log(res))
+        .catch(err => console.error(err))
+        res.json({
+          isValid: false,
+          resultCode:200
+        })
       }
+
+    },
+    function() {
+      res.json({
+        isValid: false,
+        resultCode:500
+      })
     })
 })
 app.post('/requestEmailAuthCode',(req,res)=>{
     var checkaccount='select *from user where account=?'
     var email = req.body.email
     var param = [email]
-    connection.query(checkaccount,param,function(err,result) {
-      if(err) {
+    sqlquery(checkaccount,param,function(result) {
+      if(result&&result.length) {
         res.json({
-          exist: false,
-          resultCode:500
+          exist: true,
+          resultCode:200
         })
       } else {
-        if(result&&result.length) {
-          res.json({
-            exist: true,
-            resultCode:200
-          })
-        } else {
-          var num = getCode()
-          console.log(num)
-          cache.del()
-          cache.put(email,num)
-          //이메일 전송
+        var num = getCode()
+        console.log(num)
+        cache.del()
+        cache.put(email,num)
+        //이메일 전송
 
-          sendEmail(email,'[고민앱] 메일 인증 코드 발송','인증번호는 '+num+' 입니다' )
-          res.json({
-            exist: false,
-            resultCode:200
-          })
-        }
+        sendEmail(email,'[고민앱] 메일 인증 코드 발송','인증번호는 '+num+' 입니다' )
+        res.json({
+          exist: false,
+          resultCode:200
+        })
       }
+    },
+    function() {
+      res.json({
+        exist: false,
+        resultCode:500
+      })
     })
+
 })
 app.post('/signInWithToken',accessToken,(req,res)=> {
   verifyToken(req.token,function(){
@@ -311,38 +372,35 @@ app.post('/signInWithToken',accessToken,(req,res)=> {
     console.log(account)
     var checkaccount='select *from user where account=? and platform=?'
     param=[account,platform]
-    connection.query(checkaccount,param,function(err,result) {
-      if(err) {
-        res.json({
-          signInResult:false,
-          profileWritten:false,
-          resultCode:500
-        })
-      }else { 
-        if(result&&result.length) {
-          if(result[0].nickname===null) {//닉네임 없을때
-            res.json({
-              signInResult:true,
-              profileWritten:false,
-              resultCode:200
-            })
-          } else { // 닉네임 있을때
-            res.json({
-              signInResult:true,
-              profileWritten:true,
-              resultCode:200
-            })
-          }
-        } else { 
+    sqlquery(checkaccount,param,function(result) {
+      if(result&&result.length) {
+        if(result[0].nickname===null) {//닉네임 없을때
           res.json({
-            signInResult:false,
+            signInResult:true,
             profileWritten:false,
             resultCode:200
           })
+        } else { // 닉네임 있을때
+          res.json({
+            signInResult:true,
+            profileWritten:true,
+            resultCode:200
+          })
         }
+      } else { 
+        res.json({
+          signInResult:false,
+          profileWritten:false,
+          resultCode:200
+        })
       }
+    }, function() {
+      res.json({
+        signInResult:false,
+        profileWritten:false,
+        resultCode:500
+      })
     })
-    
   })
 })
 app.post('/emailSignIn',(req,res)=>{
@@ -356,83 +414,80 @@ app.post('/emailSignIn',(req,res)=>{
   }
   var param=[account,'email']
   var checkaccount='select *from user where account=? and platform=?'
-  connection.query(checkaccount,param,function(err,result){
-    if(err) {
-      console.log(err)
-      res.json({
-        isMember:false,
-        profileWritten:false,
-        token:'a',
-        resultCode:500
-      })
-    }
-    else {
-      if(result&&result.length){ //가입된상태
-        bcrypt.compare(password,result[0].password,function(err,pwresult){
-          if(err) {
-            console.log('2')
-            res.json({
-              isMember:false,
-              profileWritten:false,
-              token:'b',
-              resultCode:500
-            })
-          } else {
-            if(pwresult) { //비밀번호 맞음(로그인성공)
-              jwt.sign({user:user},'secretkey',{expiresIn:'20d'},(err,authtoken)=>{ //jwt 발급
-                if(err) {
-                  console.log('3')
+  sqlquery(checkaccount,param,function(result) {
+    if(result&&result.length){ //가입된상태
+      bcrypt.compare(password,result[0].password,function(err,pwresult){
+        if(err) {
+          console.log('2')
+          res.json({
+            isMember:false,
+            profileWritten:false,
+            token:'b',
+            resultCode:500
+          })
+        } else {
+          if(pwresult) { //비밀번호 맞음(로그인성공)
+            jwt.sign({user:user},'secretkey',{expiresIn:'20d'},(err,authtoken)=>{ //jwt 발급
+              if(err) {
+                console.log('3')
+                res.json({
+                  isMember:true,
+                  profileWritten:false,
+                  token:'c',
+                  resultCode:500
+                })
+    
+              }else {
+                if(result[0].nickname === null) {
+                  //프로필작성 안된상태
                   res.json({
                     isMember:true,
                     profileWritten:false,
-                    token:'c',
-                    resultCode:500
+                    token:authtoken,
+                    resultCode:200
                   })
-      
+    
                 }else {
-                  if(result[0].nickname === null) {
-                    //프로필작성 안된상태
-                    res.json({
-                      isMember:true,
-                      profileWritten:false,
-                      token:authtoken,
-                      resultCode:200
-                    })
-      
-                  }else {
-                    //프로필작성 완료상태
-                    res.json({
-                      isMember:true,
-                      profileWritten:true,
-                      token:authtoken,
-                      resultCode:200
-                    })
-      
-                  }
-      
+                  //프로필작성 완료상태
+                  res.json({
+                    isMember:true,
+                    profileWritten:true,
+                    token:authtoken,
+                    resultCode:200
+                  })
+    
                 }
-              })
+    
+              }
+            })
 
-            } else { //비밀번호 틀린상태
-              res.json({
-                isMember:false,
-                profileWritten:false,
-                token:'',
-                resultCode:200
-              })
-            }
-
+          } else { //비밀번호 틀린상태
+            res.json({
+              isMember:false,
+              profileWritten:false,
+              token:'',
+              resultCode:200
+            })
           }
-        })
-      } else { //계정 존재하지않음
-        res.json({
-          isMember:false,
-          profileWritten:false,
-          token:'',
-          resultCode:200
-        })
-      }
+
+        }
+      })
+    } else { //계정 존재하지않음
+      res.json({
+        isMember:false,
+        profileWritten:false,
+        token:'',
+        resultCode:200
+      })
     }
+  },function(){
+    console.log(err)
+    res.json({
+      isMember:false,
+      profileWritten:false,
+      token:'a',
+      resultCode:500
+    })
   })
 })
 app.post('/socialSign',(req,res) => {
@@ -446,59 +501,57 @@ app.post('/socialSign',(req,res) => {
     account:account,
     platform:platform
   }
-  connection.query(checkaccount,param,function(err,result){
-    if(err) {
+  sqlquery(checkaccount,param,function(result){
+    if(result&&result.length){ //가입된상태
+      jwt.sign({user:user},'secretkey',{expiresIn:'20d'},(err,authtoken)=>{ //jwt 발급
+        if(err) {
+          res.json({
+            isMember:true,
+            profileWritten:false,
+            token:'',
+            resultCode:500
+          })
+
+        }else {
+          if(result[0].nickname === null) {
+            //프로필작성 안된상태
+            res.json({
+              isMember:true,
+              profileWritten:false,
+              token:authtoken,
+              resultCode:200
+            })
+
+          }else {
+            //프로필작성 완료상태
+            res.json({
+              isMember:true,
+              profileWritten:true,
+              token:authtoken,
+              resultCode:200
+            })
+
+          }
+
+        }
+      })
+
+    } else { //가입안된상태
       res.json({
+        isMember:false,
+        profileWritten:false,
+        token:'',
+        resultCode:200
+      })
+    }
+
+  },function(){
+     res.json({
         isMember:false,
         profileWritten:false,
         token:'',
         resultCode:500
       })
-    }
-    else {
-      if(result&&result.length){ //가입된상태
-        jwt.sign({user:user},'secretkey',{expiresIn:'20d'},(err,authtoken)=>{ //jwt 발급
-          if(err) {
-            res.json({
-              isMember:true,
-              profileWritten:false,
-              token:'',
-              resultCode:500
-            })
-
-          }else {
-            if(result[0].nickname === null) {
-              //프로필작성 안된상태
-              res.json({
-                isMember:true,
-                profileWritten:false,
-                token:authtoken,
-                resultCode:200
-              })
-
-            }else {
-              //프로필작성 완료상태
-              res.json({
-                isMember:true,
-                profileWritten:true,
-                token:authtoken,
-                resultCode:200
-              })
-
-            }
-
-          }
-        })
-
-      } else { //가입안된상태
-        res.json({
-          isMember:false,
-          profileWritten:false,
-          token:'',
-          resultCode:200
-        })
-      }
-    }
   })
 })
 app.post('/socialSignUp',(req,res)=> {
@@ -514,27 +567,25 @@ app.post('/socialSignUp',(req,res)=> {
   var param=[platform,account,phonenumber]
   var insertaccount='INSERT IGNORE INTO user(platform,account,phonenumber) VALUES (?,?,?)'
 
-  connection.query(insertaccount,param,function(err,result) {
-    if(err) {
-      res.json({
-        resultCode:500,
-        token:''
-      })
-    } else {
-      jwt.sign({user:user},'secretkey',{expiresIn:'20d'},(err,authtoken)=>{
-        if(err) {
-          res.json({
-            resultCode:500,
-            token:''
-          })
-        } else {
-          res.json({
-            resultCode:200,
-            token:authtoken
-          })
-        }
-      })
-    }
+  sqlquery(insertaccount,param,function(result){
+    jwt.sign({user:user},'secretkey',{expiresIn:'20d'},(err,authtoken)=>{
+      if(err) {
+        res.json({
+          resultCode:500,
+          token:''
+        })
+      } else {
+        res.json({
+          resultCode:200,
+          token:authtoken
+        })
+      }
+    })
+  },function(){
+    res.json({
+      resultCode:500,
+      token:''
+    })
   })
 })
 app.post('/emailSignUp',(req,res)=> {
@@ -544,19 +595,17 @@ app.post('/emailSignUp',(req,res)=> {
   var insertaccount='INSERT IGNORE INTO user(platform,account,password,phonenumber) VALUES (?,?,?,?)'
   if(req.body.authCode == cache.get(req.body.account)) {
     cache.del(req.body.email)
-    connection.query(insertaccount,param,function(err,result) {
-      if(err) {
-        console.log(err)
-        res.json({
-          resultCode:500,
-          isCorrect: false
-        })
-      } else {
-        res.json({
-          resultCode:200,
-          isCorrect: true
-        })
-      }
+    sqlquery(insertaccount,param,function(result){
+      res.json({
+        resultCode:200,
+        isCorrect: true
+      })
+    },function(){
+      console.log(err)
+      res.json({
+        resultCode:500,
+        isCorrect: false
+      })
     })
 
   } else {
@@ -588,32 +637,28 @@ app.post('/checkNickname',(req,res)=>{
   var nickname=req.body.nickname
   var checknick='select *from user where nickname=?'
   console.log(nickname)
-  connection.query(checknick,nickname,function(err,result){
-      if(err)
+  sqlquery( checknick,param,function(result) {
+    if(result.length==0)
       {
-        res.json({
-          resultCode:200,
-          isValid:false
-      })
+          res.json({
+              resultCode:200,
+              isValid:true
+          })
       }
-      else
-      {
-          if(result.length==0)
-          {
-              res.json({
-                  resultCode:200,
-                  isValid:true
-              })
-          }
-          else{
-              res.json({
-                  resultCode:200,
-                  isValid:false
-              })
-          }
+      else{
+          res.json({
+              resultCode:200,
+              isValid:false
+          })
       }
+  },function() {
+    res.json({
+      resultCode:200,
+      isValid:false
+  })
   })
 })
+var port = process.env.PORT || 3000
 app.get('/',(req,res)=>{
   res.send('hello')
 })
